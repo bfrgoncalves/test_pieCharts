827ad70775332d3ee59aa87a42d4194f07b2d82f
bfrgoncalves <brunofiliperg@gmail.com>
2015-09-23 14:23:52 +0100

    testios

--- customiosNode.js
+++ customiosNode.js
@@ -124,7 +124,7 @@ function buildCircleNodeShader() {
                         '}',
                         'else{',
 
-                            'gl_FragColor = vec4(1.0,0.0,0.0,1.0);',
+                            'gl_FragColor = vec4(0.0,1.0,0.0,1.0);',
                        '}',
                 '   } else {',
                 '     gl_FragColor = vec4(0.0);',
